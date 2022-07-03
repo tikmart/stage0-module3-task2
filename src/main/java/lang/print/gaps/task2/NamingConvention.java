a1e6dcde07741728d612747d2e8f200efb313b6e
tikmart <t.martirosyan012@gmail.com>
2022-07-03 19:04:59 +0400

    Test Push of task

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int _variable = 10;
+        int variable = 10;
         int variable_One = 1;
         int variableTwo = 2;
         int variable_three = 3;
@@ -11,5 +11,10 @@ public class NamingConvention {
         int VariableSix = 6;
         int variableseven = 7;
         int VariableEight = 8;
+
+        int x = 7;
+
+
+        System.out.println(x);
     }
 }
