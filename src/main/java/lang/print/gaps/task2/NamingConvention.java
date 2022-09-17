835586ace60fc5545fe46783d14aebb62f5ff64a
tikmart <t.martirosyan012@gmail.com>
2022-09-17 15:52:17 +0400

    push

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -2,17 +2,17 @@ package lang.print.gaps.task2;
 
 public class NamingConvention {
     public static void main(String[] args) {
-        int variable = 10;
+        int variableTen = 10;
         int variableOne = 1;
         int variableTwo = 2;
         int variableThree = 3;
-        int variableFour = 4;
-        int variableFive = 5;
+        final int variableFour = 4;
+        final int variableFive = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int VariableEight = 8;
-        int x = 7;
-        System.out.println(x);
+
+
 
 
 
